4173d292abb2394e1c0af6f0d14f2637e7099365
Asad Gilani <gillani.asad947@gmail.com>
2026-08-14 19:27:38 +0500

    feat: implement safe calculate tool

---
+++ tools.py
@@ -0,0 +1,24 @@
+import re
+import wikipedia
+
+def calculate( expression: str )->str:
+    if not re.fullmatch(r"[0-9+\-*/().\s]+", expression ):
+        return "Error: Invalid characters in the expression!!"
+    try:
+        result = eval(expression)
+        return str(result)
+    except Exception as e:
+        return f"Error evaluating expression: {e}"
+
+def wiki_search(query: str)-> str:
+    try:
+        return wikipedia.summary(query, sentences = 3)
+    except wikipedia.exceptions.PageError:
+        return f"No Wikipedia page found for '{query}'"
+    except Exception as e:
+        return f"Error: {e}"
+
+TOOLS = {
+    "calculate": calculate,
+    "Wikipedia": wiki_search
+}
